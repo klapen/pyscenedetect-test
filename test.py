5c41bd34c7ffb1868312857fe3ffc91e27d2f1a9
klapen <oasaavedrac@gmail.com>
2018-06-09 16:34:31 -0500

    Change labels to print

--- test.py
+++ test.py
@@ -8,19 +8,16 @@ detector_list = [
     scenedetect.detectors.ThresholdDetector(threshold = 16, min_percent = 0.9)
 ]
 
-video_fps, frames_read = scenedetect.detect_scenes_file(
-    path, scene_list, detector_list)
-
-# scene_list now contains the frame numbers of scene boundaries.
-print 'Scene list'
+video_fps, frames_read = scenedetect.detect_scenes_file(path, scene_list, detector_list)
+print 'Scene_list now contains the frame numbers of scene boundaries.'
 print scene_list
 
 # create new list with scene boundaries in milliseconds instead of frame #.
 scene_list_msec = [(1000.0 * x) / float(video_fps) for x in scene_list]
-print 'Scene list'
+print 'List with scene boundaries in milliseconds instead of frame'
 print scene_list_msec
 
 # create new list with scene boundaries in timecode strings ("HH:MM:SS.nnn").
 scene_list_tc = [scenedetect.timecodes.get_string(x) for x in scene_list_msec]
-print 'Scene list'
+print 'list with scene boundaries in timecode strings ("HH:MM:SS.nnn").'
 print scene_list_tc
