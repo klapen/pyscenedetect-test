0e07ce32cce9ddee3155dddaf332c81fbfccad13
klapen <oasaavedrac@gmail.com>
2018-06-09 16:20:24 -0500

    First commit

---
+++ test.py
@@ -0,0 +1,26 @@
+import scenedetect
+
+scene_list = []        # Scenes will be added to this list in detect_scenes().
+path = 'goldeneye.mp4'  # Path to video file.
+
+# Usually use one detector, but multiple can be used.
+detector_list = [
+    scenedetect.detectors.ThresholdDetector(threshold = 16, min_percent = 0.9)
+]
+
+video_fps, frames_read = scenedetect.detect_scenes_file(
+    path, scene_list, detector_list)
+
+# scene_list now contains the frame numbers of scene boundaries.
+print 'Scene list'
+print scene_list
+
+# create new list with scene boundaries in milliseconds instead of frame #.
+scene_list_msec = [(1000.0 * x) / float(video_fps) for x in scene_list]
+print 'Scene list'
+print scene_list_msec
+
+# create new list with scene boundaries in timecode strings ("HH:MM:SS.nnn").
+scene_list_tc = [scenedetect.timecodes.get_string(x) for x in scene_list_msec]
+print 'Scene list'
+print scene_list_tc
