aed4751381a57565eeeaebb09d8d7c79caab1c0c
klapen <oasaavedrac@gmail.com>
2018-06-09 18:42:20 -0500

    Create commands to test library

--- README.md
+++ README.md
@@ -1,8 +1,49 @@
 # PySceneDetect test
 
-```
-$ virtualenv -p python2.7 vtest
-$ source vtest/bin/activate
-$ pip install -r requirements.txt
-$ python test.py
-```
+Test to learn how to use [PySceneDetect](https://github.com/Breakthrough/PySceneDetect).
+
+## Dependencies
+# [Numpy](http://www.numpy.org)
+# [OpenCV](https://opencv.org)
+
+## Preparation
+
+The last version available present some problems, so [it is required to use v0.3.5](https://github.com/Breakthrough/PySceneDetect/issues/32#issuecomment-309627736). So for this is better to use PIP to install the last stable version, and not follow the proyect readme instructions. 
+
+### MacOS
+For MacOS creating a virtual enviroment to use this repo create an error. For that reason, to be able to use it, it is required to install some dependencies throught [Homebrew](https://brew.sh). The steps for MacOS are:
+
+* First ensure you have python2.7 globally installed in your computer. If not, you can get python2.7 [here](https://www.python.org).
+* Install OpenCV with [Homebrew](https://brew.sh):
+    ```
+	$ brew install opencv3
+    ```
+* Install PySceneDetect:
+    ```
+	$ pip install pyscenedetect
+    ```
+* Git clone this repo to your PC
+    ```
+        $ git clone https://github.com/klapen/pyscenedetect-test.git
+     ```
+* Ready to use the test on global terminal
+
+### Linux
+On Linux, it is straight forward to use it on virtual enviroments. To run the test:
+    ```
+        $ git clone https://github.com/klapen/pyscenedetect-test.git
+	$ cd pyscenedetect-test
+	$ virtualenv -p python2.7 vtest
+    	$ source vtest/bin/activate
+    	$ pip install -r requirements.txt
+    ```
+
+## Usage
+
+   ```
+	python test.py [options] [files list]
+   ```
+   Options and arguments:
+   * -s --save-images  : Save images for scene detection.
+   * -d --detector     : Scene detector type. Only aceptable options are 'content' (default) or 'threshold'
+   * [video file list] : List of video files to parse.
--- test.py
+++ test.py
@@ -1,23 +1,63 @@
+#!/usr/bin/python
+import os,sys,getopt
 import scenedetect
 
-scene_list = []        # Scenes will be added to this list in detect_scenes().
-path = 'my_video.mp4'  # Path to video file.
+def validateInputFiles(files):
+    files_list = []
+    for file in files:
+        os.path.exists(file) and files_list.append(file)
+    return files_list
 
-# Usually use one detector, but multiple can be used.
-detector_list = [
-    scenedetect.detectors.ThresholdDetector(threshold = 16, min_percent = 0.9)
-]
+def videoDetection(path,save_images,detector):
+    print "*-*-*- Start processing the file "+path
+    scene_list = [] # Scenes will be added to this list in detect_scenes().
 
-video_fps, frames_read = scenedetect.detect_scenes_file(path, scene_list, detector_list, save_images = True)
-print 'Scene_list now contains the frame numbers of scene boundaries.'
-print scene_list
+    # Usually use one detector, but multiple can be used.
+    if(detector == 'content'):
+        detector_list = [scenedetect.detectors.ContentDetector(threshold = 30, min_scene_len = 15)]
+    else:
+        detector_list = [scenedetect.detectors.ThresholdDetector(threshold = 16, min_percent = 0.9)]
+        
+    video_fps, frames_read = scenedetect.detect_scenes_file(path, scene_list, detector_list, save_images = save_images)
+    print 'Scene_list now contains the frame numbers of scene boundaries.'
+    print scene_list
+    
+    # create new list with scene boundaries in milliseconds instead of frame #.
+    scene_list_msec = [(1000.0 * x) / float(video_fps) for x in scene_list]
+    print 'Boundaries in milliseconds instead of frame'
+    print scene_list_msec
+    
+    # create new list with scene boundaries in timecode strings ("HH:MM:SS.nnn").
+    scene_list_tc = [scenedetect.timecodes.get_string(x) for x in scene_list_msec]
+    print 'Boundaries in timecode strings ("HH:MM:SS.nnn").'
+    print scene_list_tc
+    print 'End processing '+path+' *-*-*-'
+    
+if __name__ == '__main__':
+    argv = sys.argv[1:]
+    try:
+        opts,args = getopt.getopt(argv, "hsd:",["save-images=","detector="])
+    except getopt.GetopError:
+        print 'test.py -s -d [detector] [video file list]'
+        sys.exit(2)
+        
+    save_images = False
+    detector_type = "content"
+    for opt,arg in opts:
+        if opt == '-h':
+            print """
+            usage: python test.py [options] [files list]
+            Options and arguments:
+            -s --save-images  : Save images for scene detection.
+            -d --detector     : Scene detector type. Only aceptable options are 'content' (default) or 'threshold'
+            [video file list] : List of video files to parse.
+            """
+            sys.exit()
+        elif opt in ("-s","--save-images"):
+            save_images = True
+        elif opt in ("-d","--detector"):
+            detector_type = arg if arg in('content','threshold') else 'content'
 
-# create new list with scene boundaries in milliseconds instead of frame #.
-scene_list_msec = [(1000.0 * x) / float(video_fps) for x in scene_list]
-print 'List with scene boundaries in milliseconds instead of frame'
-print scene_list_msec
-
-# create new list with scene boundaries in timecode strings ("HH:MM:SS.nnn").
-scene_list_tc = [scenedetect.timecodes.get_string(x) for x in scene_list_msec]
-print 'list with scene boundaries in timecode strings ("HH:MM:SS.nnn").'
-print scene_list_tc
+    input_files = validateInputFiles(args)
+    for video_file in input_files:
+        videoDetection(video_file,save_images,detector_type)
